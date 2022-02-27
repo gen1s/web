0ea9fd57e355e855736874f0e7e099bf79b1c911
gen1s <88340214+gen1s@users.noreply.github.com>
2022-02-27 14:51:40 +0100

    remove obsolete files

--- coments.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$title = "Comments";
-include_once "templates/head.inc.php"?>
-<body>
-    <?php include_once "templates/navbar.inc.php"?>
-        
-    <main class = "container-fluid">
-<?php include_once "templates/end.inc.php"?>
